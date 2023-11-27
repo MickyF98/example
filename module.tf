19865b8c3c65fd1867fe5bf3733750504ee367f6
micky <fonsahmichel@gmail.com>
2023-11-27 23:06:13 +0000

    new instance type

--- module.tf
+++ module.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "myEC2" {
     ami = "ami-0cfd0973db26b893b"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     tags = {
         Name = "HelloWorld"
     }
